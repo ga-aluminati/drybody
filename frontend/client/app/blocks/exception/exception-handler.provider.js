7bf98bb80f10e29c5afbca9e7bd102afbc652482
Nathan Bloom <nabloom.dev@gmail.com>
2016-07-21 19:15:11 -0500

    Open souce refs

--- frontend/client/app/blocks/exception/exception-handler.provider.js
+++ frontend/client/app/blocks/exception/exception-handler.provider.js
@@ -3,6 +3,12 @@
 (function() {
     'use strict';
 
+/** 
+* Code originally generated with the Hottowel Generator, by John Pappa, and adapted
+* Source: Google Chrome source code https://github.com/johnpapa/generator-hottowel
+* retrieved on, July 21, 2016. 
+*/
+
     angular
         .module('blocks.exception')
         .provider('exceptionHandler', exceptionHandlerProvider)
